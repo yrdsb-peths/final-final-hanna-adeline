ceca9656561929002f149bc3f49336bb1f9188c3
Hanna11-m <440059605@gapps.yrdsb.ca>
2025-12-10 18:20:16 -0500

    Add possible background

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,10 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        // Set background
+        GreenfootImage bg = new GreenfootImage("images/background.gif");
+        bg.scale(getWidth(), getHeight());
+        setBackground(bg);
     }
 }
