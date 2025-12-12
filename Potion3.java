942911a723e48f2706db6d1571b1ced637e2bc72
Adeline Lai <adelinelai08@gmail.com>
2025-12-12 16:00:26 -0500

    Changed Potion2 images

--- Potion3.java
+++ Potion3.java
@@ -1,23 +1,23 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Potion2 here.
+ * Write a description of class Potion3 here.
  * 
  * @author Adeline
  * @version December 2025
  */
-public class Potion2 extends Actor
+public class Potion3 extends Actor
 {
-    GreenfootImage[] potionImage = new GreenfootImage[14];
+    GreenfootImage[] potionImage = new GreenfootImage[12];
     SimpleTimer animationTimer = new SimpleTimer();
-    public Potion2()
+    public Potion3()
     {
         //Initial potion 1 image
         setImage(potionImage[0]);
         
         for(int i=0; i<potionImage.length; i++)
         {
-            potionImage[i] = new GreenfootImage("images/Potions/Potion2/potion2-" + i + ".png");
+            potionImage[i] = new GreenfootImage("images/Potions/Potion3/potion3-" + i + ".png");
             potionImage[i].scale(22, 37);
         }
         
@@ -26,7 +26,7 @@ public class Potion2 extends Actor
     int imageIndex = 0;
     public void animatePotion()
     {
-        if(animationTimer.millisElapsed() < 100)
+        if(animationTimer.millisElapsed() < 50)
         {
             return;
         }
@@ -36,7 +36,7 @@ public class Potion2 extends Actor
         imageIndex = (imageIndex + 1) % potionImage.length;
     }
     /**
-     * Act - do whatever the Potion2 wants to do. This method is called whenever
+     * Act - do whatever the Potion3 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
