543dbef0cab524eb45d40949b36c686e46d8c300
Hanna11-m <440059605@gapps.yrdsb.ca>
2025-12-12 22:57:01 -0500

    Update Witch attack
    Allow left and right attack

--- Witch.java
+++ Witch.java
@@ -1,18 +1,25 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Witch here.
+ * The Witch, our main character.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Hanna & Adeline 
+ * @version December 2025
  */
 public class Witch extends Actor
 {
     // Image idles of witch
-    GreenfootImage[] defaultIdle = new GreenfootImage[37];
-    GreenfootImage[] attack1 = new GreenfootImage[13];
-    GreenfootImage[] attack2 = new GreenfootImage[26];
+    GreenfootImage[] defaultIdleRight = new GreenfootImage[37];
+    GreenfootImage[] defaultIdleLeft = new GreenfootImage[37];
+    GreenfootImage[] attack1Right = new GreenfootImage[13];
+    GreenfootImage[] attack1Left = new GreenfootImage[13];
+    GreenfootImage[] attack2Right = new GreenfootImage[26];
+    GreenfootImage[] attack2Left = new GreenfootImage[26];
     
+    // Direction the witch is facing
+    String facing = "right";
+    
+    // Variables of attacks
     boolean isAttacking1 = false;
     boolean isAttacking2 = false;
     
@@ -26,27 +33,48 @@ public class Witch extends Actor
      */
     public Witch()
     {
-        for(int i = 0; i < defaultIdle.length; i++)
+        // Set idle image for default witch
+        for(int i = 0; i < defaultIdleRight.length; i++)
         {
-            defaultIdle[i] = new GreenfootImage("witch_default_idle/default" + i + ".png");
-            defaultIdle[i].scale(300, 300);
+            defaultIdleRight[i] = new GreenfootImage("witch_default_idle/default" + i + ".png");
+            defaultIdleRight[i].scale(300, 300);
+        }
+        for(int i = 0; i < defaultIdleLeft.length; i++)
+        {
+            defaultIdleLeft[i] = new GreenfootImage("witch_default_idle/default" + i + ".png");
+            defaultIdleLeft[i].mirrorHorizontally();
+            defaultIdleLeft[i].scale(300, 300);
         }
         
-        for(int i = 0; i < attack1.length; i++)
+        // Set idle image for attack 1 of witch
+        for(int i = 0; i < attack1Right.length; i++)
         {
-            attack1[i] = new GreenfootImage("witch_attack1_idle/attack1_" + i + ".png");
-            attack1[i].scale(300, 300);
+            attack1Right[i] = new GreenfootImage("witch_attack1_idle/attack1_" + i + ".png");
+            attack1Right[i].scale(300, 300);
+        }
+        for(int i = 0; i < attack1Left.length; i++)
+        {
+            attack1Left[i] = new GreenfootImage("witch_attack1_idle/attack1_" + i + ".png");
+            attack1Left[i].mirrorHorizontally();
+            attack1Left[i].scale(300, 300);
         }
         
-        for(int i = 0; i < attack2.length; i++)
+        // Set idle image for attack 2 of witch
+        for(int i = 0; i < attack2Right.length; i++)
         {
-            attack2[i] = new GreenfootImage("witch_attack2_idle/attack2_" + i + ".png");
-            attack2[i].scale(300, 300);
+            attack2Right[i] = new GreenfootImage("witch_attack2_idle/attack2_" + i + ".png");
+            attack2Right[i].scale(300, 300);
+        }
+        for(int i = 0; i < attack2Left.length; i++)
+        {
+            attack2Left[i] = new GreenfootImage("witch_attack2_idle/attack2_" + i + ".png");
+            attack2Left[i].mirrorHorizontally();
+            attack2Left[i].scale(300, 300);
         }
         defaultTimer.mark();
         
         // Initial witch image
-        setImage(defaultIdle[0]);
+        setImage(defaultIdleRight[0]);
     }
     
     /**
@@ -61,8 +89,16 @@ public class Witch extends Actor
         }
         defaultTimer.mark();
         
-        setImage(defaultIdle[imageIndex]);
-        imageIndex = (imageIndex + 1) % defaultIdle.length;
+        if(facing.equals("right"))
+        {
+            setImage(defaultIdleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % defaultIdleRight.length;
+        }
+        else
+        {
+            setImage(defaultIdleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % defaultIdleLeft.length;
+        }
     }
     
     /**
@@ -77,8 +113,16 @@ public class Witch extends Actor
         }
         attackTimer1.mark();
         
-        setImage(attack1[attackIndex1]);
-        attackIndex1 = attackIndex1 + 1;
+        if(facing.equals("right"))
+        {
+            setImage(attack1Right[attackIndex1]);
+            attackIndex1 = attackIndex1 + 1;
+        }
+        else
+        {
+            setImage(attack1Left[attackIndex1]);
+            attackIndex1 = attackIndex1 + 1;
+        }
     }
     
     /**
@@ -93,8 +137,16 @@ public class Witch extends Actor
         }
         attackTimer2.mark();
         
-        setImage(attack2[attackIndex2]);
-        attackIndex2 = attackIndex2 + 1;
+        if(facing.equals("right"))
+        {
+            setImage(attack2Right[attackIndex2]);
+            attackIndex2 = attackIndex2 + 1;
+        }
+        else
+        {
+            setImage(attack2Left[attackIndex2]);
+            attackIndex2 = attackIndex2 + 1;
+        }
     }
     
     public void act()
@@ -102,10 +154,12 @@ public class Witch extends Actor
         if(Greenfoot.isKeyDown("left"))
         {
             move(-2);
+            facing = "left";
         }
         else if(Greenfoot.isKeyDown("right"))
         {
             move(2);
+            facing = "right";
         }
         
         // Start attack 1
@@ -131,7 +185,7 @@ public class Witch extends Actor
         if(isAttacking1)
         {
             animateAttackOne();
-            if(attackIndex1 >= attack1.length)
+            if(attackIndex1 >= attack1Right.length)
             {
                 isAttacking1 = false;
                 attackIndex1 = 0;
@@ -140,7 +194,7 @@ public class Witch extends Actor
         else if(isAttacking2)
         {
             animateAttackTwo();
-            if(attackIndex2 >= attack2.length)
+            if(attackIndex2 >= attack2Right.length)
             {
                 isAttacking2 = false;
                 attackIndex2 = 0;
