276171a21164f04cba7e1a37d3614cecdb80dd8a
Adeline Lai <adelinelai08@gmail.com>
2025-12-16 19:29:43 -0500

    added SlimeRed to Level1 world

--- MyWorld.java
+++ MyWorld.java
@@ -12,5 +12,8 @@ public class MyWorld extends World {
         // Create the witch object
         Witch witch = new Witch();
         addObject(witch, 120, 195);
+        
+        Level1SlimeRed slimered = new Level1SlimeRed();
+        addObject(slimered, 500,250);
     }
 }
