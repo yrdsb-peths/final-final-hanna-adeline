cd7ba55aacbb8e89083a9f1333e7e1af6d1077fb
Adeline Lai <adelinelai08@gmail.com>
2026-01-08 13:44:40 -0500

    added level1/2/3Start boolean

--- MyWorld.java
+++ MyWorld.java
@@ -1,6 +1,21 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+<<<<<<< Updated upstream
+=======
+    public static boolean level1Complete = false;
+    public static boolean level2Complete = false;
+    public static boolean level3Complete = false;
+    
+    public static boolean potion1Collected = false;
+    public static boolean potion2Collected = false;
+    public static boolean potion3Collected = false;
+    
+    public static boolean level1Start = false;
+    public static boolean level2Start = false;
+    public static boolean level3Start = false;
+    
+>>>>>>> Stashed changes
     public MyWorld() {
         super(600, 400, 1);
         
